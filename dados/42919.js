6cd2f5df291973a78315ba8429059692b242484d
RicardoBreganti <73093098+RicardoBreganti@users.noreply.github.com>
2020-10-26 20:17:41 -0300

    Atualizado por Ricardo Breganti

--- dados/42919.js
+++ dados/42919.js
@@ -1,6 +1,6 @@
 export default {
-    "repositorio": "joaocarloslima",
-    "nome": "João Carlos",
-    "bio": "Professor da ETEC Irmã Agostina e da FIAP. Desenvolvedor Java, Javascript, PHP. Coordenador do Curso de Desenvolvimento de Sistemas. Diretor Acadêmico."
+    "repositorio": "RicardoBreganti",
+    "nome": "20651",
+    "bio": "Aluno de Desenvolvimento de Sistemas."
 }
 
