4e912bd5d18ea106c3169e6831c8e5a23b842a0e
augusto <augusto@debian>
2020-10-26 20:22:37 -0300

    Adicionei o meu RM na pasta dados/ com minha descrição.

---
+++ dados/20633.js
@@ -0,0 +1,6 @@
+export default {
+    "repositorio": "augv137",
+    "nome": "Augusto Viníius",
+    "bio": "Estudante de Desenvolvimento de Sistemas na ETEC Irma Agostina. Jovem de 19 anos que aspira aprender Desenvolvimento de Software e seguir carreira em Segurança da Informação."
+}
+
