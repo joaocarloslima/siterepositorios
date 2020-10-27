47832c88bbb16a313478ec76c9c97a89510e8abd
Andressa <andsm_jac7@hotmail.com>
2020-10-27 20:19:14 -0300

    git commit

---
+++ dados/20848.js
@@ -0,0 +1,5 @@
+export default {
+    "usuario": "andressasmpacheco",
+    "nome": "Andressa Simões Medeiros Pacheco",
+    "bio": "Estudante de técnico em desenvolvimento de sistemas na Etec Irmã Agostina."
+}
