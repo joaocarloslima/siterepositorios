d208e698e086a265f98c4a632d9d3f06553b9b5e
Paulo Henrique <paulolvs6@gmail.com>
2020-10-27 11:06:39 -0300

    Atualização de Paulo Henrique

---
+++ dados/20635.js
@@ -0,0 +1,6 @@
+export default {
+    "repositorio": "Pauloxyz",
+    "nome": "Paulo Henrique",
+    "bio": "Estudante de Desenvolvimento de Sistemas na ETEC Irma Agostina. Finalizando o Ensino Médio, e pretendendo cursar faculdade de Engenharia da Computação."
+}
+
