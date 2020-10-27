1cc9368afab056822677d103c76bed8348327aac
GuilhermeOtavio <gmartinellifragoso@gmail.com>
2020-10-27 11:40:15 -0300

    Alteração do Guilherme

---
+++ dados/20843.js
@@ -0,0 +1,6 @@
+export default {
+    "repositorio": "GuilhermeOtavio",
+    "nome": "Guilherme Otavio Martinelli Fragoso",
+    "bio": "Aluno da ETEC Irmã Agostina do curso de Desenvolvimento de Sistemas."
+}
+
