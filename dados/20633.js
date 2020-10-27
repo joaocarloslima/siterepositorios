d2e2712c2079f35a26e50d3ec9c4b5b286e37575
joaocarloslima <joao@etecia.com.br>
2020-10-27 16:40:27 -0300

    :up: adicionando cards

--- dados/20633.js
+++ dados/20633.js
@@ -1,5 +1,5 @@
 export default {
-    "repositorio": "https://github.com/augv137",
+    "repositorio": "augv137",
     "nome": "Augusto Viníius",
     "bio": "Estudante de Desenvolvimento de Sistemas na ETEC Irma Agostina. Jovem de 19 anos que aspira aprender Desenvolvimento de Software e seguir carreira em Segurança da Informação."
 }
--- data.js
+++ data.js
@@ -2,8 +2,8 @@ import rm42919 from "./dados/42919.js"
 //import rm20644 from "./dados/20644.js"
 //import rm20647 from "./dados/20647.js"
 //import rm20848 from "./dados/20848.js"
-//import rm20633 from "./dados/20633.js"
-//import rm20638 from "./dados/20638.js"
+import rm20633 from "./dados/20633.js"
+import rm20638 from "./dados/20638.js"
 //import rm20228 from "./dados/20228.js"
 //import rm20655 from "./dados/20655.js"
 //import rm20634 from "./dados/20634.js"
@@ -15,27 +15,27 @@ import rm42919 from "./dados/42919.js"
 //import rm19339 from "./dados/19339.js"
 //import rm20840 from "./dados/20840.js"
 //import rm20628 from "./dados/20628.js"
-//import rm20843 from "./dados/20843.js"
-//import rm20645 from "./dados/20645.js"
+import rm20843 from "./dados/20843.js"
+import rm20645 from "./dados/20645.js"
 //import rm20183 from "./dados/20183.js"
 //import rm20636 from "./dados/20636.js"
 //import rm20853 from "./dados/20853.js"
-//import rm20650 from "./dados/20650.js"
+import rm20650 from "./dados/20650.js"
 //import rm20630 from "./dados/20630.js"
 //import rm20652 from "./dados/20652.js"
-//import rm20104 from "./dados/20104.js"
-//import rm20648 from "./dados/20648.js"
+import rm20104 from "./dados/20104.js"
+import rm20648 from "./dados/20648.js"
 //import rm20629 from "./dados/20629.js"
-//import rm20635 from "./dados/20635.js"
+import rm20635 from "./dados/20635.js"
 //import rm20851 from "./dados/20851.js"
 //import rm20841 from "./dados/20841.js"
 //import rm20842 from "./dados/20842.js"
-//import rm20651 from "./dados/20651.js"
+import rm20651 from "./dados/20651.js"
 //import rm20889 from "./dados/20889.js"
 //import rm20642 from "./dados/20642.js"
 //import rm20847 from "./dados/20847.js"
 //import rm20639 from "./dados/20639.js"
-//import rm20627 from "./dados/20627.js"
+import rm20627 from "./dados/20627.js"
 //import rm20632 from "./dados/20632.js"
 
 
@@ -43,8 +43,8 @@ var data = [ rm42919,
 //    rm20644,
 //    rm20647,
 //    rm20848,
-//    rm20633,
-//    rm20638,
+    rm20633,
+    rm20638,
 //    rm20228,
 //    rm20655,
 //    rm20634,
@@ -56,27 +56,27 @@ var data = [ rm42919,
 //    rm19339,
 //    rm20840,
 //    rm20628,
-//    rm20843,
-//    rm20645,
+    rm20843,
+    rm20645,
 //    rm20183,
 //    rm20636,
 //    rm20853,
-//    rm20650,
+    rm20650,
 //    rm20630,
 //    rm20652,
 //    rm20104,
-//    rm20648,
+    rm20648,
 //    rm20629,
-//    rm20635,
+    rm20635,
 //    rm20851,
 //    rm20841,
 //    rm20842,
-//    rm20651,
+    rm20651,
 //    rm20889,
 //    rm20642,
 //    rm20847,
 //    rm20639,
-//    rm20627,
+    rm20627,
 //    rm20632
 ]
 
