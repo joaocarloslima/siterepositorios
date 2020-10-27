c6d8afd7cd8fc1948e24bcf97bb82d890ddb8fb3
Ayrton222 <ayrton.cabani67@gmail.com>
2020-10-26 21:11:04 -0300

    Modificação Ayrton

---
+++ dados/20638.js
@@ -0,0 +1,6 @@
+export default {
+    "repositorio": "Ayrton222",
+    "nome": "Ayrton Senna Cabani bastos",
+    "bio": "Estudante de Desenvolvimento de Sistemas Na ETEC Irmâ Agostina. Jovem de 17 anos!"
+}
+
