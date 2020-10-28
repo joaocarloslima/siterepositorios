175374ee20cf33c49c44e47905a0266f841a6404
Ayrton222 <ayrton.cabani67@gmail.com>
2020-10-27 22:54:31 -0300

    modificado por Carollina

---
+++ dados/20634.js
@@ -0,0 +1,5 @@
+export default {
+    "repositorio": "Carollina-Nuncio",
+    "nome": "Carollina Nuncio",
+    "bio": "Aluna de Desenvolvimento de Sistemas da Etec Irmã Agostina, 16 anos, 2° do ensino médio.."
+}
